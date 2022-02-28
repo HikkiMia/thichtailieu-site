2fe851b6ccca957dfb8ae690e2e1df2701604977
nguyentuan1696 <nguyentuan1686@gmail.com>
2022-02-28 23:08:23 +0700

    🐱 update

---
+++ components/Logo/index.jsx
@@ -0,0 +1,81 @@
+import styles from "./styles.module.css";
+
+const Logo = () => {
+  return (
+    <svg viewBox="0 0 3 2" width="50" height="33.33">
+      <polygon fill="#2de0e0" points="0 2 0 0 2 0">
+        <animate
+          attributeName="points"
+          calcMode="spline"
+          dur="4s"
+          keySplines="
+        .25 .1 .25 1;
+        .25 .1 .25 1;
+        .25 .1 .25 1;
+        .25 .1 .25 1;
+        .25 .1 .25 1;
+        .25 .1 .25 1"
+          keyTimes="
+        0;
+        .1;
+        .2;
+        .3;
+        .7;
+        .8;
+        1"
+          repeatCount="indefinite"
+          values="
+        0 2 0 0 2 0;
+        0 2 0 0 2 0;
+        0 2 2 2 2 0;
+        0 2 2 2 0 0;
+        0 2 2 2 0 0;
+        0 2 0 0 2 0;
+        0 2 0 0 2 0"
+        />
+      </polygon>
+      <polygon fill="#ff004f" points="1 2 1 0 3 0">
+        <animate
+          attributeName="points"
+          calcMode="spline"
+          dur="4s"
+          keySplines="
+        .25 .1 .25 1;
+        .25 .1 .25 1;
+        .25 .1 .25 1;
+        .25 .1 .25 1;
+        .25 .1 .25 1;
+        .25 .1 .25 1;
+        .25 .1 .25 1;
+        .25 .1 .25 1;
+        .25 .1 .25 1"
+          keyTimes="
+        0;
+        .1;
+        .3;
+        .4;
+        .5;
+        .6;
+        .7;
+        .8;
+        .9;
+        1"
+          repeatCount="indefinite"
+          values="
+        1 2 1 0 3 0;
+        2 2 0 2 2 0;
+        2 2 0 2 2 0;
+        2 2 0 0 2 0;
+        2 2 0 0 0 2;
+        2 2 2 0 0 2;
+        0 0 2 0 0 2;
+        0 0 2 0 0 2;
+        1 0 3 0 1 2;
+        1 0 3 0 1 2"
+        />
+      </polygon>
+    </svg>
+  );
+};
+
+export default Logo;
